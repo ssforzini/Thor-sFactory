e2ab059905757fb1806417acd3573b68b9005c4f
sebastian sforzini <sebasforzini@hotmail.com>
2018-08-26 21:15:59 -0300

    Agregado del script para el main menu. Hay que hacer que se pueda usar el teclado de la PC y un joistick. Esto va a servir para el juego

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+	
+	[HideInInspector]
+	public int selectedLevel;
+
+	// Use this for initialization
+	void Start () {
+		selectedLevel = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	private bool loadScene = false;
+
+	[SerializeField]
+	private int scene;
+	private GameObject loadingText;
+	private MainMenu wc;
+	private int alreadyLoad;
+
+	void Start(){
+		alreadyLoad = 0;
+		loadingText = GameObject.Find ("Loading");
+		if(loadingText != null){
+			loadingText.SetActive (false);	
+		}
+		wc = GameObject.Find ("MainMenuManager").GetComponent<MainMenu> ();
+	}
+
+	// Updates once per frame
+	void Update() {
+		if (loadScene == false && wc.selectedLevel != 0) {
+			loadScene = true;
+			if(loadingText != null){
+				loadingText.SetActive(true);
+			}
+			StartCoroutine(LoadNewScene());
+		}
+	}
+
+	IEnumerator LoadNewScene() {
+		
+		if(SceneManager.GetActiveScene().name == "Main Menu" && alreadyLoad == 0){
+			alreadyLoad = 1;
+			AsyncOperation async = null;
+			string levelToLoad = "Level" + wc.selectedLevel;
+			Application.LoadLevelAsync (levelToLoad);
+			loadScene = false;
+			// While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+			while (!async.isDone) {
+				yield return null;
+			}
+		}
+	}
+}
