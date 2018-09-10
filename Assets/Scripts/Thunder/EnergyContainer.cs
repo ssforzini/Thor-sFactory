aa3d0ad378f2a37540a385b370796d9ca32836ef
ssforzini <sebastiansforzini@gmail.com>
2018-09-10 17:55:02 -0300

    Se agrego el script del contenedor de energia. Todavia falta desarrollar todo por completo.

---
+++ Assets/Scripts/Thunder/EnergyContainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eType{
+	movable,notMovable
+}
+
+public class EnergyContainer : MonoBehaviour {
+
+	public float range;
+	public eType type;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
